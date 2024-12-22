f854ed27c68b3cb91b43c9cc5883ed522d2f7e6e
Alina <151466607+volohaalina@users.noreply.github.com>
2024-12-22 20:10:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:"https://volohaalina.github.io/cv",
+  base:"https://volohaalina.github.io/CV",
   server: {
     port: 5173,
     open: true,
